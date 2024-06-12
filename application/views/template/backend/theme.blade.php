29d9c2e6f9c4d45118d7c884e5cc18742b5d3baf
Sriayuu <sriayupalangda@gmail.com>
2024-06-12 22:22:05 +0800

    perubahan dan tambahan fitur edit profile

--- application/views/template/backend/theme.blade.php
+++ application/views/template/backend/theme.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- Favicons -->
   <!--<link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
 <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
@@ -258,6 +258,12 @@
                 Table
               </a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="{{site_url('backend/user/profile') }}">
+              <i class="bi bi-person-circle" style="margin-right: 4px;"></i>
+                Update Profile
+              </a>
+           </li>
             <li class="nav-item">
               <a  class="nav-link d-flex align-items-center gap-2" href="{{ site_url ('logout')}}">Sign out</a>
             </li>
